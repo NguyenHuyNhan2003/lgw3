6cca6b252ded874d2ea14f10a640fed13cc39fd0
NguyenHuyNhan2003 <124428721+NguyenHuyNhan2003@users.noreply.github.com>
2025-02-10 11:47:15 +0700

    Update ip4multi_listener.py

--- receiver/ip4multi_listener.py
+++ receiver/ip4multi_listener.py
@@ -1,4 +1,5 @@
-import socketimport struct
+import socket
+import struct
 
 MULTICAST_GROUP = '224.0.0.1'  
 PORT = 5002
@@ -8,6 +9,7 @@ sock.bind(('', PORT))
 
 mreq = struct.pack("4sl", socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-print(f"Listening for Multicast messages on {MULTICAST_GROUP}:{PORT}...")while True:
+print(f"Listening for Multicast messages on {MULTICAST_GROUP}:{PORT}...")
+while True:
     data, addr = sock.recvfrom(1024)
     print(f"Received multicast from {addr}: {data.decode()}")
